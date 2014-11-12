a5cad0985662904770b4b30480fbd982fc0ecd14
Jamrocklord <kolade.siyanbola.692@my.aquinas.me>
2014-11-12 11:00:40 -0500

    Added to resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,5 +5,6 @@ var formattedName= HTMLheaderName.replace("%data%", name);
 
 var role= "Developer";
 var formattedRole= HTMLheaderRole.replace("%data%",role);
-
+var skills=["Programming in Javascript, Python, Pascal and ADB"," ",",Technician"," ",",Car lover"];
 $("#header").append(formattedName, formattedRole);
+$("#main").append(skills);
