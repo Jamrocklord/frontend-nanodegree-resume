c287e16bbe3a7a0bc2e78abbb9c4b74f07f0055f
Jamrocklord <kolade.siyanbola.692@my.aquinas.me>
2014-11-13 02:13:28 -0800

    Added bio

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,6 +5,11 @@ var formattedName= HTMLheaderName.replace("%data%", name);
 
 var role= "Developer";
 var formattedRole= HTMLheaderRole.replace("%data%",role);
-var skills=["Programming in Javascript, Python, Pascal and ADB"," ",",Technician"," ",",Car lover"];
-$("#header").append(formattedName, formattedRole);
-$("#main").append(skills);
+var skills= [" Skills:"," ","Programming in Javascript, Python, Pascal and ADB"," ",",also I am a technician able to fix computers and hand held devices."," ",",I am also a big car lover"];
+var bio ={
+  "name": name,
+  "role": role,
+  "skills": skills
+};
+$("#header").append(formattedName, formattedRole,skills);
+$("#main").append(bio);
