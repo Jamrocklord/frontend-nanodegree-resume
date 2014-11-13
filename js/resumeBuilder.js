94df116269b9b2329ba8f5a9e11ac155b7696686
Jamrocklord <kolade.siyanbola.692@my.aquinas.me>
2014-11-13 10:59:08 -0500

    added to bio and image

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,10 +6,20 @@ var formattedName= HTMLheaderName.replace("%data%", name);
 var role= "Developer";
 var formattedRole= HTMLheaderRole.replace("%data%",role);
 var skills= [" Skills:"," ","Programming in Javascript, Python, Pascal and ADB"," ",",also I am a technician able to fix computers and hand held devices."," ",",I am also a big car lover"];
+
 var bio ={
   "name": name,
+  "age": 17,
   "role": role,
   "skills": skills
 };
-$("#header").append(formattedName, formattedRole,skills);
-$("#main").append(bio);
+var contact_info= {
+      "Address": "226 Creek Path Ave. Oakville, ON, Canada",
+      "Postal Code": "L6L-6T5",
+      "Phone Number": "(416)371-4045",
+      "E-Mail": "kool.siyanbola@gmail.com"
+  };
+  
+var FormattedContact=  HTMLcontactGeneric.replace("%contact%",contact_info);
+$("#header").append(formattedName, formattedRole);
+$("#main").append(FormattedContact);
