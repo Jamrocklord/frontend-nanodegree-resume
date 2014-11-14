4d26efa99b87b37a0194c4dff0f50dd63a0bff03
Jamrocklord <kolade.siyanbola.692@my.aquinas.me>
2014-11-14 11:01:24 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	js/resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,44 +5,51 @@ var formattedName= HTMLheaderName.replace("%data%",name);
 
 var role= "Developer";
 var formattedRole= HTMLheaderRole.replace("%data%",role);
-$("#header").append(formattedName, formattedRole);
+$("#header").prepend(formattedName, formattedRole);
 
 var skills= ["Skills: Programming in Javascript, Python, Pascal and ADB. \n\
 also I am a technician able to fix computers and hand held devices. I am also a big car lover"];
-
+ 
 var bio ={
+  "name": name,
   "age": 17,
   "role": role,
   "skills": skills,
-<<<<<<< HEAD
-};
-var FormattedContact=  HTMLcontactGeneric.replace("%contact%", bio);
-$("#header").append(formattedName, formattedRole);
-=======
   "welcome_message": "Welcome, thank you for viewing this web page. This web page shows my resume.",
-  "bio_image": "images/Profile_Picture.jpg",
-  "contact_info": {
+  "bio_image": "images/Profile_Picture.jpg"
+};
+var contact_info= {
       "Address": "226 Creek Path Ave. Oakville, ON, Canada",
       "Postal Code": "L6L-6T5",
-      "Phone_number": "(416)371-4045",
+      "Phone_number": "(416) 371-4045",
       "email": "kool.siyanbola@gmail.com",
       "github": "JamrockLord"
-    }
-  };
+    };
   var education= {
     "Elementary school": "Liberty Acadamy At The Proiry",  
-    "High school": "St. Thomas Aquinas Catholic Secondary School"
-      
+    "High school": "St. Thomas Aquinas Catholic Secondary School"      
 };
   
-var Formattedmobile= HTMLmobile.replace("%data%",bio[contact_info.Phone_number]);
-var Formattedemail= HTMLemail.replace("%data%",bio[contact_info.email]);
-var FormattedContact=  HTMLcontactGeneric.replace("%contact%",bio[contact_info.Address]);
-var Formattedgithub= HTMLgithub.replace("%data%",bio.github);
-var Formattedage= bio.age;
+var Formattedmobile= HTMLmobile.replace("%data%",contact_info.Phone_number);
+var Formattedemail= HTMLemail.replace("%data%",contact_info.email);
+var Formattedgithub= HTMLgithub.replace("%data%",contact_info.github);
+var Formatttebioimage= HTMLbioPic.replace("%data%",bio.bio_image);
+var formattedGeneric = HTMLcontactGeneric.replace("%data%", Formattedmobile + Formattedemail + Formattedgithub);
+
+$("#topContacts").append(formattedGeneric);
+
+
+
+
+
+
+//var Formattedage= bio.age;
+
+/*
 $("#header").append(Formattedmobile);
 $("#header").append(Formattedemail);
-$("#header").append(Formattedgithub);
->>>>>>> origin/master
 $("#main").append(FormattedContact);
-$("#header").append(Formattedage);
+$("#header").prepend(Formattedimage);
+$("#header").append(Formattedgithub);
+
+*/
