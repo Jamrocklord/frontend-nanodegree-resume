98406ce1976081f211b18b2fc61a328ff2fca80b
Jamrocklord <kolade.siyanbola.692@my.aquinas.me>
2014-11-17 01:21:42 -0800

    Added more objects and edited image

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,7 +5,7 @@ var formattedName= HTMLheaderName.replace("%data%",name);
 
 var role= "Student";
 var formattedRole= HTMLheaderRole.replace("%data%",role);
-$("#header").prepend(formattedName, formattedRole);
+
 
 var skills= ["Skills: Programming in Javascript, Python, Pascal and ADB. \n\
 also I am a technician able to fix computers and hand held devices. I am also a big car lover"];
@@ -16,7 +16,7 @@ var bio ={
   "role": role,
   "skills": skills,
   "welcome_message": "Welcome, thank you for viewing this web page. This web page shows my resume.",
-  "bio_image": "images/Profile_Picture.jpg"
+   "bio_image": "images/Profile_Picture.jpg"
 };
 var contact_info= {
       "Address": "226 Creek Path Ave. Oakville, ON, Canada",
@@ -24,26 +24,50 @@ var contact_info= {
       "Phone_number": "(416) 371-4045",
       "email": "kool.siyanbola@gmail.com",
       "github": "JamrockLord"
-      }
+      };
   var education= {
-    "Elementary school": "Liberty Acadamy At The Proiry",  
-    "High school": "St. Thomas Aquinas Catholic Secondary School"      
-};
-  var work= {
-      
+      "Schools": [
+          {
+    "City":"Kingston, Jamaica",
+    "Elementary_school": "Liberty Acadamy At The Proiry",
+    "High_school": "St. George's College",
+    "Deploma":"High School Deploma",
+    "Subjects": "Math, History, Literature, English, Visual Arts, Computer Science, Religous Education"
+    },
+    {
+     "City": "Oakville, Ontario, Canada",
+     "High_school_two": "St. Thomas Aquinas Catholic Secondary School",
+     "Deploma":"High School Deploma",
+     "Subjects": "Math, History, English, Religous Education, Computer Science",
+     "Oniline_courses":"Udacity Javasript basics course"
+    }
+      ]
   };
+    var work= {
+      "job_one": "Professional Druming",
+      "job_ two": "Technician for handheld devices"
+  };
+  
+  var projects= {
+      "Title": "",
+      "Employer": "",
+      "Date": "",
+      "Discription": ""
+  };
+  
 var Formattedmobile= HTMLmobile.replace("%data%",contact_info.Phone_number);
 var Formattedemail= HTMLemail.replace("%data%",contact_info.email);
 var FormattedContact=  HTMLcontactGeneric.replace("%contact%",contact_info.Address);
 var Formattedgithub= HTMLgithub.replace("%data%", contact_info.github);
 var Formattedage= bio.age;
-
-var Formattedgithub= HTMLgithub.replace("%data%",contact_info.github);
 var Formatttebioimage= HTMLbioPic.replace("%data%",bio.bio_image);
 var formattedGeneric = HTMLcontactGeneric.replace("%data%", Formattedmobile + Formattedemail + Formattedgithub);
 
+
+
+$("#header").prepend(formattedName, formattedRole);
 $("#topContacts").append(formattedGeneric);
-$("#header").prepend();
+$("#main").prepend(Formatttebioimage);
 
 
 
